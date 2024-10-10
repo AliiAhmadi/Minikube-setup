6e99c52ed8ba55306b57e6a9471d400129453e01
Lexur <107758775+AliiAhmadi@users.noreply.github.com>
2024-10-10 07:15:34 -0400

    Add files via upload

---
+++ minikube.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+###################
+## Author: Lexur ##
+###################
+
+check_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Unable to detect Linux distribution."
+        exit 1
+    fi
+}
+
+install_docker() {
+    echo "Installing Docker..."
+
+    if [[ "$DISTRO" == "fedora" || "$DISTRO" == "rhel" ]]; then
+        sudo dnf install -y dnf-plugins-core
+        sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+        sudo dnf install -y docker-ce docker-ce-cli containerd.io
+        sudo systemctl start docker
+        sudo systemctl enable docker
+    elif [[ "$DISTRO" == "ubuntu" || "$DISTRO" == "debian" ]]; then
+        sudo apt-get update -y
+        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update -y
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+        sudo systemctl start docker
+        sudo systemctl enable docker
+    else
+        echo "Unsupported distribution."
+        exit 1
+    fi
+}
+
+install_kubectl() {
+    echo "Installing Kubectl..."
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+}
+
+install_minikube() {
+    echo "Installing Minikube..."
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    rm minikube-linux-amd64
+}
+
+configure_minikube() {
+    echo "Configuring Minikube..."
+    minikube config set memory 4096
+    minikube config set cpus 2
+    minikube config set image-pull-timeout 300
+}
+
+clean_up() {
+    echo "Cleaning up Docker and Minikube..."
+    minikube delete
+    docker system prune -a -f
+}
+
+start_minikube() {
+    echo "Starting Minikube with Docker driver..."
+    minikube start --driver=docker --docker-opt="default-ulimit=nofile=65536:65536" --image-repository="docker.haiocloud.com"
+}
+
+check_minikube_status() {
+    echo "Checking Minikube status..."
+    minikube status
+
+    echo "Fetching Minikube logs..."
+    minikube logs
+}
+
+main() {
+    check_distro
+    install_docker
+    install_kubectl
+    install_minikube
+    configure_minikube
+    clean_up
+    start_minikube
+    check_minikube_status
+}
+
+main
+
